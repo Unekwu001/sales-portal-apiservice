fe5c4ddfea9c26cea1f2f71211bc057eb8521f1f
Theophilus Unekwu <theophilusu@examroom.ai>
2024-07-17 10:24:16 +0100

    changed connection string

--- Api/Program.cs
+++ Api/Program.cs
@@ -28,11 +28,13 @@ var app = builder.Build();
 app.ApplyDatabaseMigrations();
 
 //Configuring the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
